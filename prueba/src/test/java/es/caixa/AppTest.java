fac5615717c56570d7608d6d500aac7fd0b34e69
Ubuntu <ubuntu@ip-172-31-40-94.eu-west-1.compute.internal>
2020-06-16 12:55:40 +0000

    maven

--- prueba/src/test/java/es/caixa/AppTest.java
+++ prueba/src/test/java/es/caixa/AppTest.java
@@ -32,3 +32,15 @@ public class AppTest extends TestCase{
         assertEquals(resultado,-5);
     }
 }
+// Paso 1: Montar pruebas automatizadas: Casos de prueba. Este fichero
+// ------- DEVOPS 
+// Paso 2 [IC]: Montar un servidor de CI (Jenkins): Instalar el codigo de los desarrolladores
+           // Entorno de integración y ejecutar automaticamente las pruebas
+           // Bamboo (Atlassian: Jira, Confluence, Bitbucket)
+           // Jetbrains ( Intellij, android studio): TeamCity
+           // TravisCI
+           // Azure devops
+// Paso 3 [CD]: Continuous Delivery: Jenkins (Entrega continua)
+    // Tomar los que está instalado dentro integración, y llevarlo a un repo de artifactos:
+    // Nexus, Artefactory
+// Paso 4 [CD]: Continuous Deployment: Instalación en un entorno productivo
